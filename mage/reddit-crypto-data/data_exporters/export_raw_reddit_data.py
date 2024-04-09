2b667a473b0ef51e190674d7488b1e81345d939c
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-04-09 16:47:11 +0200

    created a pipeline for scraping reddit data and uploading it to the gcs bucket

---
+++ mage/reddit-crypto-data/data_exporters/export_raw_reddit_data.py
@@ -0,0 +1,29 @@
+import os
+
+import pyarrow as pa
+import pyarrow.parquet as pq
+from pandas import DataFrame
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/personal-gcp.json"
+
+project_id = os.environ['PROJECT_ID']
+bucket_name = os.environ['BUCKET_NAME']
+
+table_name = "reddit-raw-data"
+root_path = f'{bucket_name}/{table_name}'
+
+@data_exporter
+def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
+
+    pa_table = pa.Table.from_pandas(df)
+    gcs_fs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table=pa_table,
+        root_path=root_path,
+        partition_cols=['date-by-day'],
+        filesystem=gcs_fs
+    )
