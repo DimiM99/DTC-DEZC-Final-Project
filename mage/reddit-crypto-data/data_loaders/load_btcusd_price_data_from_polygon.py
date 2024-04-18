16f004aa9fd1088ac224e289a11409cffea26cba
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-04-18 16:26:50 +0200

    changed btc price-data pipline to include 2 years of data

--- mage/reddit-crypto-data/data_loaders/load_btcusd_price_data_from_polygon.py
+++ mage/reddit-crypto-data/data_loaders/load_btcusd_price_data_from_polygon.py
@@ -12,10 +12,10 @@ if 'test' not in globals():
 def load_data_from_api(*args, **kwargs):
 
     end_date = time.time()
-    start_date = end_date - 30 * 24 * 60 * 60
+    start_date = end_date - 60*60*24*365*2
     api_key = os.environ['POLYGON_API_KEY']
 
-    url = f'https://api.polygon.io/v2/aggs/ticker/X:BTCUSD/range/1/day/{start_date}/{end_date}?adjusted=true&sort=asc&limit=50000&apiKey={api_key}'
+    url = f'https://api.polygon.io/v2/aggs/ticker/X:BTCUSD/range/1/day/2022-04-18/2024-04-17?adjusted=true&sort=asc&limit=50000&apiKey={api_key}'
     response = requests.get(url)
     data = response.json()['results']
     result = pd.DataFrame(data)
